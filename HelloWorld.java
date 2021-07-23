e40f3245d30118c0c14220a13266c3cd8291e556
Parth Shah <parthshah590@gmail.com>
2021-07-23 21:14:38 +0530

    Separator added in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World!");
-	System.out.println("");
+	System.out.println("----------------");
 	System.out.println("Lucky ones here");
     }
 }
